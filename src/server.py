8ded3d29acc012d32c92ad886d751e7afbb0eb71
Michelle <54164456+oldhorizons@users.noreply.github.com>
2025-02-14 02:00:43 +1000

    osc made into a class. More or less done mvp

--- src/server.py
+++ src/server.py
@@ -8,38 +8,45 @@ from functools import cached_property
 from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+from test_server_osc import OSCCommunicator
+from constants import ip, http_port
+from queue import Queue
 
 model = pp.Starburst()
-x, y, w, h = 0, 0, 0, 0
-
-def process_request(json):
-    #extract data
-    byteData = json['image']
-    xDim = json['xdim']
-    yDim = json['ydim']
-
-    #convert json back to readable image encoding
-    nums = base64.decodebytes(byteData)
-    npArray = np.frombuffer(nums, dtype=np.float64)
-    np.reshape(npArray, (xDim, yDim))
-    img = npArray.astype('uint8')
-    plt.imshow(img)
-    plt.show()
-
-    # if ROI not initialised, initialise
-    if w == 0:
-        x, y, w, h = cv2.selectROI(img)
-    cv2.destroyAllWindows()
-    img_crop = img[y:y+h, x:x+w]
-    cv2.imshow("cropped", img_crop)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    grayscale = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-    pupil = model.run(grayscale)
-    print(pupil)
-
+x, y, w, h = 0, 0, 0, 0    
 
 class TrackerServer(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.oscCommunicator = OSCCommunicator()
+
+    def process_request(self, json):
+        #extract data
+        byteData = json['image']
+        xDim = json['xdim']
+        yDim = json['ydim']
+
+        #convert json back to readable image encoding
+        nums = base64.decodebytes(byteData)
+        npArray = np.frombuffer(nums, dtype=np.float64)
+        np.reshape(npArray, (xDim, yDim))
+        img = npArray.astype('uint8')
+        plt.imshow(img)
+        plt.show()
+
+        # if ROI not initialised, initialise
+        if w == 0:
+            x, y, w, h = cv2.selectROI(img)
+        cv2.destroyAllWindows()
+        img_crop = img[y:y+h, x:x+w]
+        cv2.imshow("cropped", img_crop)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        grayscale = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+        pupil = model.run(grayscale)
+        # self.oscCommunicator.actionableQueue.put(pupil)
+
+    
     def post_data(self):
         content_length = int(self.headers.get("Content-Length", 0))
         return self.rfile.read(content_length)
@@ -49,7 +56,7 @@ class TrackerServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
-        response = process_request(self.post_data())
+        response = self.process_request(self.post_data())
         self.wfile.write(response.encode("utf-8"))
         print("GOTTEM")
 
@@ -58,8 +65,8 @@ class TrackerServer(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     print("starting")
-    server = HTTPServer(("192.168.0.72", 5005), TrackerServer)
+    server = HTTPServer((ip, http_port), TrackerServer)
     print("server initialised")
     server.serve_forever()
-    print("serving on 192.68.0.72:5005")
+    print(f"serving on {ip}:{http_port}")
     
--- src/test_server_osc.py
+++ src/test_server_osc.py
@@ -2,23 +2,60 @@ import argparse
 import random
 import time
 import threading
+from queue import Queue
 
 from pythonosc import udp_client
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
 import constants
+# https://github.com/attwad/python-osc/blob/main/examples/simple_2way.py
 ip = constants.ip
 server_port = constants.osc_server_port
 client_port = constants.osc_client_port
 
+class OSCCommunicator:
+    def __init__(self, ip, server_port, client_port):
+        self.ip = ip
+        self.server_port = server_port
+        self.client_port = client_port
+        # listen to addresses and print changes in values
+        self.dispatcher = Dispatcher()
+        self.dispatcher.map("/1/echo", self.server_echo_handler, "Echo")
+        self.dispatcher.map("/ping", print)
+        self.dispatcher.map("/message", self.server_message_handler, "Messager")
+        self.actionableQueue = Queue()
+        self.messageQueue = Queue()
+      
+    def server_echo_handler(self, unused_addr, args, value):
+        print("received: " + args)
+        print(value)
+    
+    def server_message_handler(self, unused_addr, args, value):
+        self.actionableQueue.put(args)
+        
+    def start_server(self):
+        print("Starting Server")
+        self.server = osc_server.ThreadingOSCUDPServer((self.ip, self.server_port), self.dispatcher)
+        print(f"Serving on {self.server.server_address}")
+        thread = threading.Thread(target=self.server.serve_forever)
+        thread.start()
+    
+    def start_client(self):
+        print("Starting Client")
+        self.client = udp_client.SimpleUDPClient(self.ip, self.client_port)
+        # print("Sending on {}".format(client.))
+        thread = threading.Thread(target=self.send_message(self.client, "hello world!", "/1/echo"))
+        thread.start()
 
-def print_fader_handler(unused_addr, args, value):
-    print(f"[{args[0]}] ~ {value:0.2f}")
+    def send_message(self, message, endpoint):
+        while True:
+            endpoint, message = self.messageQueue.get()
+            self.client.send_message(endpoint, message)
+            self.messageQueue.task_done()
 
-
-def print_xy_fader_handler(unused_addr, args, value1, value2):
-    print(f"[{args[0]}] ~ {value2:0.2f} ~ {value1:0.2f}")
-
+    def start(self):
+        self.start_server()
+        self.start_client()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,41 +76,3 @@ if __name__ == "__main__":
         help="The port the OSC Client is listening on",
     )
     args = parser.parse_args()
-
-    # listen to addresses and print changes in values
-    dispatcher = Dispatcher()
-    dispatcher.map("/1/push2", print)
-    dispatcher.map("/1/fader1", print_fader_handler, "Focus")
-    dispatcher.map("/1/fader2", print_fader_handler, "Zoom")
-    dispatcher.map("/1/xy1", print_xy_fader_handler, "Pan-Tilt")
-    dispatcher.map("/ping", print)
-
-
-def start_server(ip, port):
-    print("Starting Server")
-    server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
-    print(f"Serving on {server.server_address}")
-    thread = threading.Thread(target=server.serve_forever)
-    thread.start()
-
-
-def start_client(ip, port):
-    print("Starting Client")
-    client = udp_client.SimpleUDPClient(ip, port)
-    # print("Sending on {}".format(client.))
-    thread = threading.Thread(target=random_values(client))
-    thread.start()
-
-
-# send random values between 0-1 to the three addresses
-def random_values(client):
-    while True:
-        for x in range(10):
-            client.send_message("/1/fader2", random.random())
-            client.send_message("/1/fader1", random.random())
-            client.send_message("/1/xy1", [random.random(), random.random()])
-            time.sleep(0.5)
-
-
-start_server(args.serverip, args.serverport)
-start_client(args.clientip, args.clientport)
